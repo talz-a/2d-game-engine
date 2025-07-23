0f5a014a26294f3b0b672f374830f7a784c468a6
talz <talal.ahm25@gmail.com>
2025-07-23 19:25:35 -0400

    added entity operator overloading

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,23 +1,15 @@
 #include "ECS.hpp"
 
-int Entity::GetId() const {
-    return id;
-}
+int Entity::GetId() const { return id; }
 
-void System::AddEntityToSystem(Entity entity) {
-    entities.push_back(entity);
-}
+void System::AddEntityToSystem(Entity entity) { entities.push_back(entity); }
 
 void System::RemoveEntityFromSystem(Entity entity) {
     std::erase_if(entities, [&entity](Entity other) {
-        return entity.GetId() == other.GetId();
+        return entity == other;
     });
 }
 
-std::vector<Entity> System::GetSystemEntities() const {
-    return entities;
-}
+std::vector<Entity> System::GetSystemEntities() const { return entities; }
 
-const Signature& System::GetComponentSignature() const {
-    return componentSignature;
-}
+const Signature& System::GetComponentSignature() const { return componentSignature; }
